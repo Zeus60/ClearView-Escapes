aa05c67dca1b21838aa30a3e96099d0564f9a579
Kevin <kbeck1960@gmail.com>
2021-02-16 17:57:32 -0500

    change to package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ const postCSSPlugins = [
   require("postcss-mixins"),
   require("postcss-simple-vars"),
   require("postcss-nested"),
-  require("postcss-hexrgba"),
+  //require("postcss-hexrgba"),
   require("autoprefixer"),
 ];
 
